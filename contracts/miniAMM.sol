c06295242b3d859bebf86a3804da8d3c20f32a76
"Godhanded" <"aezeigwe2003@gmail.com">
2022-06-12 15:27:30 -0700

    lets see again

--- contracts/miniAMM.sol
+++ contracts/miniAMM.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.5;
 
 contract playAMM{
     constructor(){
-
+        owner=msg.sender;
     }
     uint256 netEth;
     uint256 netGtoken;
+    uint256 k;
+
+    address owner;
 
 
 
@@ -14,6 +17,7 @@ contract playAMM{
     {
         netEth+=eth;
         netGtoken+=amount;
+        if(msg.sender==owner) k = amount*eth;
     }
 
     function swap(uint256 amount) external
