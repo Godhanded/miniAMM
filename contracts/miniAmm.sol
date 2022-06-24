d0477b2b20571d3040abbaf3b22ab7c02aefbc90
Godand <aezeigwe2003@gmail.com>
2022-06-24 16:22:26 +0000

    finished deposit

--- contracts/miniAmm.sol
+++ contracts/miniAmm.sol
@@ -1,27 +1,54 @@
-//SPDX-License-Identifier: 
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.5;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract playAMM{
-    constructor(){
+    constructor(address tokenAddr){
         owner=msg.sender;
+        token= tokenAddr;
     }
     uint256 netEth;
-    uint256 netGtoken;
+    uint256 netGtoken; 
     uint256 k;
 
     address owner;
+    address token;
 
-
-
+    error failed(string);
+    /**
+      @dev using constant product algorith, limited to only one token for now,
+           must have approved contract to spend, on front-end?
+      @notice constant product algo: X * Y = k*/
     function deposit(uint256 amount, uint256 eth) external payable 
     {
-        netEth+=eth;
-        netGtoken+=amount;
-        if(msg.sender==owner) k = amount*eth;
+        uint256 newEth= netEth + eth;
+        uint256 newGtoken= netGtoken + amount;
+        uint256 newk= newEth*newGtoken;
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+        if(newk >= k)
+        {
+            (newk>k)? withk(amount, eth, newk): withoutk(amount, eth);
+        }else{
+            revert failed("Must be less thank k");
+        }
+
+        
     }
 
     function swap(uint256 amount) external
     {
 
+    }
+
+    function withk(uint256 amount, uint256 eth, uint256 newk) internal
+    {
+        netEth+=eth;
+        netGtoken+=amount;
+        k=newk;
+    }
+    function withoutk(uint256 amount, uint256 eth) internal
+    {
+        netEth+=eth;
+        netGtoken+=amount;
     }
 }
